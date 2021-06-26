00a55c226ac9bcde5990b104ae91acf0f0d0743f
ALEN MANOJ <alen.manojrny@gmail.com>
2021-06-26 10:21:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+#
+num=int(input("enter the number"))
+flag=False
+if num>1:
+    for i in range(2,num):
+        if(num%i)==0:
+        flag=True
+        break
+if flag:
+    print(num, "is not a prime number")
+else:
+    print(num,"is a prime number")
+
+
+
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
